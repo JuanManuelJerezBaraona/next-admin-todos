871eb9e307e19912f22fe33e1101c234e0a12d70
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2024-04-03 15:52:06 -0300

    Add yup validation for PUT and POST requests

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,5 +1,6 @@
 import prisma from '@/lib/prisma'
 import { NextResponse, NextRequest } from 'next/server'
+import * as yup from 'yup';
 
 export async function GET(request: Request) {
 
@@ -22,11 +23,20 @@ export async function GET(request: Request) {
     return NextResponse.json(todos);
 }
 
+const postSchema = yup.object({
+    description: yup.string().required(),
+    complete: yup.boolean().optional().default(false), //! TODO mostrar algo interesante
+})
+
 export async function POST(request: Request) { 
 
-    const body = await request.json();
-
-    const todo = await prisma.todo.create({ data: body });
-
-    return NextResponse.json(todo);
+    try {
+        const { complete, description } = await postSchema.validate( await request.json() );
+        const todo = await prisma.todo.create({ data: { complete, description } });
+    
+        return NextResponse.json(todo);
+    } catch (error) {
+        return NextResponse.json( error, { status: 400 } );
+        
+    }
 }
