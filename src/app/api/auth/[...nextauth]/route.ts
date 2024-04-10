9a15c5dfebde3d06742737143f8cdff37e7958a2
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2024-04-10 08:17:09 -0400

    Add password field to User model in schema.prisma and implement password-based authentication in route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,7 @@ import prisma from "@/lib/prisma";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import { Adapter } from "next-auth/adapters";
+
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
@@ -9,6 +10,7 @@ import CredentialsProvider from "next-auth/providers/credentials";
 export const authOptions: NextAuthOptions = {
     adapter: PrismaAdapter(prisma) as Adapter,
     providers: [
+
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID ?? '',
             clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
@@ -21,7 +23,24 @@ export const authOptions: NextAuthOptions = {
 
         CredentialsProvider({
             name: "Credentials",
-            
+            credentials: {
+                username: { label: "Correo electrónico", type: "email", placeholder: "usuario@email.com" },
+                password: { label: "Contraseña", type: "password", placeholder: "******" }
+            },
+            async authorize(credentials, req) {
+                // Add logic here to look up the user from the credentials supplied
+                const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
+
+                if (user) {
+                    // Any object returned will be saved in `user` property of the JWT
+                    return user
+                } else {
+                    // If you return null then an error will be displayed advising the user to check their details.
+                    return null
+
+                    // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+                }
+            }
         }),
     ],
 
@@ -37,7 +56,7 @@ export const authOptions: NextAuthOptions = {
 
         async jwt({ token, user, account, profile }) {
             const dbUser = await prisma.user.findUnique({ where: { email: token.email ?? 'no-email' } });
-            if ( dbUser?.isActive === false ) {
+            if (dbUser?.isActive === false) {
                 throw Error('Usuario no está activo');
             }
 
@@ -49,7 +68,7 @@ export const authOptions: NextAuthOptions = {
 
         async session({ session, token, user }) {
 
-            if( session && session.user ) {
+            if (session && session.user) {
                 session.user.roles = token.roles;
                 session.user.id = token.id;
             }
